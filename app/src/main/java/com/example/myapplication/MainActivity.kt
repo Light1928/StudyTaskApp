39d07f9aeaf91bca495a70c446e40fe75a822767
Light <ghost1928ps@gmail.com>
2020-12-12 01:21:27 +0900

    スワイプでの削除機能実装

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,16 +15,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
 import io.realm.RealmObject.deleteFromRealm
+import io.realm.RealmObject.getRealm
 import io.realm.RealmQuery
 import io.realm.RealmResults
 import io.realm.Sort
 import io.realm.kotlin.where
 import io.realm.log.RealmLog.remove
+import kotlinx.android.synthetic.main.activity_entry.view.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 
 class MainActivity : AppCompatActivity() {
-    open var id = "1"
+  //  open var id = "1"
     private lateinit var adapter: TaskAdapter
     private val realm: Realm by lazy {
         Realm.getDefaultInstance()
@@ -44,20 +47,8 @@ class MainActivity : AppCompatActivity() {
         val taskList = readAll()
         TestButton.setOnClickListener { onTestButtonTapped(it) }
 
-        // realm = Realm.getDefaultInstance()
-        //  list.layoutManager = LinearLayoutManager(this)
-        //表示処理？
-        //val task = realm.where<Task>().findAll()
-        adapter = TaskAdapter(this, taskList, object : TaskAdapter.OnItemSwipeListener{
-//            override fun onItemSwipe(item: Task) {
-//
-//                Log.d("*******",item.id)
-//                delete(item.id)
-//
-//             //   adapter.notifyItemRemoved(TaskAdapter.ViewHolder.a)
-//               // Toast.makeText(applicationContext, "を削除しました", Toast.LENGTH_SHORT).show()
-//            }
-        },true)
+        adapter = TaskAdapter(this, taskList,true)
+
 
 
         list.setHasFixedSize(true)
@@ -127,31 +118,17 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//val id = Recycler
+//println("***************")\
 
-//スワイプ時に背景が残るのは、データベースの削除ができていないため
-                //連番とリストの配置が同じなら削除できるようにした
-
-              fun onItemSwipe(item: Task) {
-
-                  Log.d("*******", item.id)
-                  delete(item.id)
-              }
-
-                adapter.notifyItemRemoved(TaskAdapter.ViewHolder.adapter)
-                Toast.makeText(applicationContext, "を削除しました", Toast.LENGTH_SHORT).show()
-            }
-
-//              realm.executeTransaction {
-//
-//
-//
-//                    val task = realm.where(Task::class.java).equalTo("id",id).findFirst()
-//                        ?: return@executeTransaction
-//                    task.deleteFromRealm()
-//                }
-                val tasks = list.adapter
+                 val view = viewHolder.itemView
+                val test = view.idTextView.text.toString()
+                println("***************************"+test)
 
                 adapter.notifyItemRemoved(viewHolder.adapterPosition)
+                delete(test)
+             //   adapter.notifyDataSetChanged()
+                list.adapter = adapter
                 Toast.makeText(applicationContext, "を削除しました", Toast.LENGTH_SHORT).show()
 
             }
@@ -179,19 +156,19 @@ class MainActivity : AppCompatActivity() {
 
 
                 //アイコンよう
-//                val deleteIcon = AppCompatResources.getDrawable(
-//                    this@MainActivity,
-//                    R.drawable.ic_baseline_delete_36
-//                )
-//                val iconMarginVertical =
-//                    (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
-//
-//                deleteIcon.setBounds(
-//                    itemView.left + iconMarginVertical,
-//                    itemView.top + iconMarginVertical,
-//                    itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
-//                    itemView.bottom - iconMarginVertical
-//                )
+                val deleteIcon = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_baseline_delete_24
+                )
+                val iconMarginVertical =
+                    (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
+
+                deleteIcon.setBounds(
+                    itemView.left + iconMarginVertical,
+                    itemView.top + iconMarginVertical,
+                    itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
+                    itemView.bottom - iconMarginVertical
+                )
                 background.setBounds(
                     itemView.left,
                     itemView.top,
@@ -199,114 +176,10 @@ class MainActivity : AppCompatActivity() {
                     itemView.bottom
                 )
                 background.draw(c)
-//                deleteIcon.draw(c)
+                deleteIcon.draw(c)
             }
         })
 }
-//    fun delete(id: Long) {
-//        realm.executeTransaction {
-//            val task = realm.where(Task::class.java).equalTo("id", id).findFirst()
-//                ?: return@executeTransaction
-//            task.deleteFromRealm()
-//        }
-//    }
-//
-//    fun delete(task: Task) {
-//        realm.executeTransaction {
-//            task.deleteFromRealm()
-//        }
-//    }
-//
-//    fun readAll(): RealmResults<Task> {
-//        return realm.where(Task::class.java).findAll()
-//    }
-//
-//
-//    fun onTestButtonTapped(view: View?) {
-//        val intent = Intent(this, EntryActivity::class.java)
-//        startActivity(intent)
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        realm.close()
-//    }
-//
-//
-//    fun getSwipeToDismissTouchHelper(adapter: TaskAdapter) =
-//        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-//            ItemTouchHelper.ACTION_STATE_IDLE,
-//            ItemTouchHelper.RIGHT
-//        ) {
-//            override fun onMove(
-//                recyclerView: RecyclerView,
-//                viewHolder: RecyclerView.ViewHolder,
-//                target: RecyclerView.ViewHolder
-//            ): Boolean {
-//                return false
-//            }
-//
-//            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-//
-//                val id = intent?.getLongExtra("id", -1L)
-//                realm.executeTransaction {
-//                    val task = realm.where(Task::class.java).equalTo("id", id).findFirst()
-//                        ?: return@executeTransaction
-//                    task.deleteFromRealm()
-//                }
-//                Toast.makeText(applicationContext, "を削除しました", Toast.LENGTH_SHORT).show()
-//
-//            }
-//
-//            override fun onChildDraw(
-//                c: Canvas,
-//                recyclerView: RecyclerView,
-//                viewHolder: RecyclerView.ViewHolder,
-//                dX: Float,
-//                dY: Float,
-//                actionState: Int,
-//                isCurrentlyActive: Boolean
-//            ) {
-//                super.onChildDraw(
-//                    c,
-//                    recyclerView,
-//                    viewHolder,
-//                    dX,
-//                    dY,
-//                    actionState,
-//                    isCurrentlyActive
-//                )
-//                val itemView = viewHolder.itemView
-//                val background = ColorDrawable(Color.RED)
-//
-//
-//                //アイコンよう
-////                val deleteIcon = AppCompatResources.getDrawable(
-////                    this@MainActivity,
-////                    R.drawable.ic_baseline_delete_36
-////                )
-////                val iconMarginVertical =
-////                    (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
-////
-////                deleteIcon.setBounds(
-////                    itemView.left + iconMarginVertical,
-////                    itemView.top + iconMarginVertical,
-////                    itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
-////                    itemView.bottom - iconMarginVertical
-////                )
-//                background.setBounds(
-//                    itemView.left,
-//                    itemView.top,
-//                    itemView.right + dX.toInt(),
-//                    itemView.bottom
-//                )
-//                background.draw(c)
-////                deleteIcon.draw(c)
-//            }
-
-
-
-
 
 
 
--- app/src/main/java/com/example/myapplication/TaskAdapter.kt
+++ app/src/main/java/com/example/myapplication/TaskAdapter.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Context
 import android.text.format.DateFormat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,11 +14,12 @@ import io.realm.RealmRecyclerViewAdapter
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.list_item.view.*
+import org.w3c.dom.Text
 
 class TaskAdapter(
    private val context: Context,
     private var taskList: OrderedRealmCollection<Task>?,
-   private var listener: OnItemSwipeListener,// ---------追加----------
+  // private var listener: OnItemSwipeListener,// ---------追加----------
     private val autoUpdate: Boolean
 ) :
 RealmRecyclerViewAdapter<Task,TaskAdapter.ViewHolder>(taskList,autoUpdate){
@@ -36,6 +38,7 @@ RealmRecyclerViewAdapter<Task,TaskAdapter.ViewHolder>(taskList,autoUpdate){
     class ViewHolder(cell: View) : RecyclerView.ViewHolder(cell) {
         val title: TextView = cell.findViewById(R.id.contentTextView)
         val date:  TextView = cell.findViewById(R.id.dateTextView)
+        val id:  TextView = cell.findViewById(R.id.idTextView)
 
     }
 //
@@ -56,28 +59,32 @@ RealmRecyclerViewAdapter<Task,TaskAdapter.ViewHolder>(taskList,autoUpdate){
         //val imageView: ImageView = view.imageView
         val contentTextView: TextView = view.contentTextView
         val dateTextView: TextView = view.dateTextView
+        val id: TextView = view.idTextView
     }
 //
     override fun onBindViewHolder( holder: TaskAdapter.ViewHolder,position: Int){
 
-        val task: Task? = getItem(position)
-    listener.onItemSwipe(Task())
+        val task: Task? = taskList?.get(position)
+    //デバッグ用
+println(task)
+    println(position)
     val id = task?.id
+    //println("*****"+id)
+
         holder.title.text = task?.title
         holder.date.text = DateFormat.format("yyyy/mm/dd", task?.date)
-    //holder.itemView.set
-    }
+    holder.id.text = task?.id
 
-//    override fun getItemId(position: Int): Long{
-//        return getItem(position)?.id ?:0
+//    fun getId(position: Int):Int{
+//        println("*********ABC"+position)
+//        return position
 //    }
+    //holder.itemView.set
 
-    interface OnItemSwipeListener {
-        fun onItemSwipe(item: Task)
+
     }
-    interface test{
-        fun getId(item: Task)
-    }
+
+
 
 
 
