1f2973fd91c006b8bacb6ba19d4415d4ac34c00f
futa29 <fu50ta@ezweb.ne.jp>
2021-01-19 15:36:57 +0900

    fragment内に処理を記述する

--- app/src/main/java/com/example/myapplication/EntryFragment.kt
+++ app/src/main/java/com/example/myapplication/EntryFragment.kt
@@ -1,13 +1,21 @@
 package com.example.myapplication
 
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 class EntryFragment:Fragment(){
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     return inflater.inflate(R.layout.activity_entry,container,false)
     }
+
+    
+
+
 }
